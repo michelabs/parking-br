1f336610eda9e9b9535fc23e0482cededdac94e1
Michel Silva <michel.barbosa@hotmail.com>
2022-08-19 07:55:09 -0300

    Initial commit

---
+++ src/main/java/br/com/mabs/parking/ParkingBrApplication.java
@@ -0,0 +1,13 @@
+package br.com.mabs.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBrApplication.class, args);
+	}
+
+}
